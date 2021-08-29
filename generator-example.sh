a93e908db84941e78c5d2d93a719c4ae4a51b7a4
Neradoc <neraOnGit@ri1.fr>
2021-08-29 19:39:49 +0200

    update generator example script test

--- generator-example.sh
+++ generator-example.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 python3 -m generator -f _xml/keything-us.xml -o
-python3 generator/keycode_testing.py _build/generated/keycode_win_us.py generator/keycode_us_ref.py
+python3 generator/testing_keycode.py _build/generated/keycode_win_us.py generator/keycode_us_ref.py
+python3 generator/testing_layout.py _build/generated/keyboard_layout_win_us.py generator/keyboard_layout_us_ref.py
