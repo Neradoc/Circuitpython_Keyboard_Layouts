8998d85c22e440e28a370ffc0742d392dcb1540d
Neradoc <neraOnGit@ri1.fr>
2021-09-01 17:38:36 +0200

    move modules to subfolders

--- tests/validated-generated.sh
+++ tests/validated-generated.sh
@@ -11,6 +11,6 @@ for LL in $LISTE; do
 	echo https://kbdlayout.info/kbd$FILE + _${FILE}_ _${LANG}_
 	# python3 -m generator -k _xml/win/kbdlayout-info-$lang.xml -o
 	python3 -m generator -o -k https://kbdlayout.info/kbd"$FILE" --lang "$LANG"
-	cp _build/generated/keyboard_layout_win_$LANG.py libraries/
-	cp _build/generated/keycode_win_$LANG.py libraries/
+	cp _build/generated/keyboard_layout_win_$LANG.py libraries/layouts/
+	cp _build/generated/keycode_win_$LANG.py libraries/keycodes/
 done
