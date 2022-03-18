55dfa19e00f3cd249d5c3f6ba0bee4c66f404f7e
Neradoc <neraOnGit@ri1.fr>
2022-03-18 16:21:28 +0100

    huh wrong import in the generator !

--- generator/layout_generator.py
+++ generator/layout_generator.py
@@ -63,7 +63,8 @@ COMMON_HEADER_COPYRIGHT = """# SPDX-FileCopyrightText: 2021 Neradoc NeraOnGit@ri
 \"\"\"
 This file was automatically generated using Circuitpython_Keyboard_Layouts
 \"\"\"
-
+"""
+COMMON_HEADER_VERSION = """
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/Neradoc/Circuitpython_Keyboard_Layouts.git"
@@ -470,7 +471,8 @@ def make_layout_file(layout_data):
     """make the layout file contents"""
     output_file_data = (
         COMMON_HEADER_COPYRIGHT
-        + "from keyboard_layout import KeyboardLayoutBase\n"
+        + "from adafruit_hid.keyboard_layout_base import KeyboardLayoutBase\n"
+        + COMMON_HEADER_VERSION
         + f"class KeyboardLayout(KeyboardLayoutBase):\n"
         "    ASCII_TO_KEYCODE = (\n"
     )
@@ -526,7 +528,7 @@ def output_layout_file(output_file, output_file_data):
 
 def make_keycode_file(layout_data):
     """make the keycode file contents"""
-    output_file_data = COMMON_HEADER_COPYRIGHT + "class Keycode:\n"
+    output_file_data = COMMON_HEADER_COPYRIGHT + COMMON_HEADER_VERSION + "class Keycode:\n"
 
     def ck(x):
         l = x[0]
